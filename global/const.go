41a65603ef787aee0cbf9ddc2d5b636d5952c226
Vitalitypi <779958365@qq.com>
2023-02-14 17:36:17 +0800

    第一次

---
+++ global/const.go
@@ -0,0 +1,11 @@
+package global
+
+//system相关的常量
+const CmdLength = 12
+const Version = 1
+const DBName = "HSRBDS_%s.db"
+const TableName = "blockchain"
+
+const Protocol = "tcp"
+const RecentBlockName_User = "recentUser"
+const RecentBlockName_Train = "recentTrain"
